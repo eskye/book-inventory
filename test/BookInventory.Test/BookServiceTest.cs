b82ecbfb1fd1007b6f1e5711721d3d4bd63b002d
eskye <sunkanmiijatuyi@gmail.com>
2020-05-17 20:14:57 +0100

    implemented book endpoints

--- test/BookInventory.Test/BookServiceTest.cs
+++ test/BookInventory.Test/BookServiceTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Threading.Tasks;
 using BookInventory.DataLayer.Repositories;
@@ -91,6 +92,78 @@ namespace BookInventory.Test
 
             await Assert.ThrowsAsync<Exception>(() => sut.UpdateBook(null, 0));
 
+        }
+
+        [Fact]
+        public async Task BookServiceTest_DeleteBook_Successful()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var bookRepoMock = new Mock<IBookRepository>();
+
+            var sut = new BookService(bookRepoMock.Object, unitOfWorkMock.Object);
+
+            unitOfWorkMock.Setup(b => b.BeginTransaction(It.IsAny<IsolationLevel>()));
+
+            bookRepoMock.Setup(c => c.GetAsync(It.IsAny<long>())).Returns(Task.FromResult(new Book()));
+
+            bookRepoMock.Setup(c => c.DeleteAsync(It.IsAny<Book>(), It.IsAny<bool>())).Returns(Task.CompletedTask);
+
+            unitOfWorkMock.Setup(c => c.Commit());
+
+            await sut.DeleteBook(1);
+
+        }
+
+
+        [Fact]
+        public async Task BookServiceTest_DeleteBook_ThrowException_If_Book_Not_found()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var bookRepoMock = new Mock<IBookRepository>();
+
+            var sut = new BookService(bookRepoMock.Object, unitOfWorkMock.Object);
+
+            unitOfWorkMock.Setup(b => b.BeginTransaction(It.IsAny<IsolationLevel>()));
+
+            unitOfWorkMock.Setup(c => c.Commit());
+
+            unitOfWorkMock.Setup(c => c.Rollback());
+
+           var response = await Assert.ThrowsAsync<Exception>(() => sut.DeleteBook(0));
+
+           Assert.Equal(response.Message, $"The book you are trying to delete does not exist");
+        }
+
+        [Fact]
+        public async Task BookServiceTest_Return_List_Of_Books()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var bookRepoMock = new Mock<IBookRepository>();
+
+            IReadOnlyList<Book> books = new List<Book>();
+
+            var sut = new BookService(bookRepoMock.Object, unitOfWorkMock.Object);
+            bookRepoMock.Setup(c => c.GetListOfBooks()).Returns(Task.FromResult(books));
+
+            var result = await sut.GetListOfBooks();
+
+            Assert.Equal(result, new List<BookListDto>()); 
+        }
+
+        [Fact]
+        public async Task BookServiceTest_Return_List_Of_Books_When_Sarch()
+        {
+            var unitOfWorkMock = new Mock<IUnitOfWork>();
+            var bookRepoMock = new Mock<IBookRepository>();
+
+            IReadOnlyList<Book> books = new List<Book>();
+
+            var sut = new BookService(bookRepoMock.Object, unitOfWorkMock.Object);
+            bookRepoMock.Setup(c => c.SearchBook(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.FromResult(books));
+
+            var result = await sut.SearchBook("testQuery", "Author");
+
+            Assert.Equal(result, new List<BookListDto>());
         }
     }
 }
